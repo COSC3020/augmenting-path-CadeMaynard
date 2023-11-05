2c50ead2cd02fc5a167a62d67a6bb96f9d5a1078
CadeMaynard <143521268+CadeMaynard@users.noreply.github.com>
2023-11-04 22:38:10 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,20 @@
 function augmentingPath(graph, start, end) {
-    return [];
+    //console.log(start)
+    if(start == end)
+        return [end];
+    arr = new Array();
+
+    for(i=0; i < Object.keys(graph[start]).length; i++) {
+        if(graph[start][Object.keys(graph[start])[i]] != 0) {
+            graph[start][Object.keys(graph[start])[i]] = 0;
+            //console.log(graph, "Deleted element")
+            arr = [start].concat(augmentingPath(graph, Object.keys(graph[start])[i], end))
+        } /*else
+            return [];*/
+    }
+    if(arr.includes(end))
+        return arr;
+    else 
+        return [];
 }
+
